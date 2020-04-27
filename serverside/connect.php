41134f9a784215079c068a168199bb7e95c91efa
Daniel Trowbridge <blujupiter32@gmail.com>
2020-04-27 22:00:54 +0100

    Register new IDs to instance

--- serverside/connect.php
+++ serverside/connect.php
@@ -1,3 +1,13 @@
 <?php
-echo rand();
+$inst = fopen("instance.txt", "a+");
+if ($inst && flock($inst, LOCK_EX)) {
+    $ids = explode(PHP_EOL, fread($inst, 2048));
+    do {
+        $id = rand();
+    } while (in_array($id, $ids));
+    fwrite($inst, $id . PHP_EOL);
+    flock($inst, LOCK_UN);
+    fclose($inst);
+}
+echo $id;
 ?>
