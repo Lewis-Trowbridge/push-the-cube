e615a05fd8107483983184f06908767c74d6773b
Daniel Trowbridge <blujupiter32@gmail.com>
2020-04-29 21:35:43 +0100

    Remove outdated ID availability check

--- serverside/connect.php
+++ serverside/connect.php
@@ -1,10 +1,7 @@
 <?php
 $inst = fopen("instance.csv", "a+");
 if ($inst && flock($inst, LOCK_EX)) {
-    $ids = explode(PHP_EOL, fread($inst, 2048));
-    do {
-        $id = rand();
-    } while (in_array($id, $ids));
+    $id = rand();
     fputcsv($inst, [$id, $_POST["character"]]);
     flock($inst, LOCK_UN);
     fclose($inst);
